b73ac0c16c0d3ee388e850794e08f82b5f41b4e6
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-04-21 13:55:51 -0400

    Fix repeated recursive creation of syminks

--- modelbuild.sh
+++ modelbuild.sh
@@ -1044,6 +1044,8 @@ for reg_type in "${_arg_stages[@]}"; do
 
 done
 
-ln -srf ${_arg_output_dir}/${reg_type}/$((i - 1)) ${_arg_output_dir}/final
+if [[ ! -L ${_arg_output_dir}/final ]]; then
+    ln -srf ${_arg_output_dir}/${reg_type}/$((i - 1)) ${_arg_output_dir}/final
+fi
 
 # ] <-- needed because of Argbash
