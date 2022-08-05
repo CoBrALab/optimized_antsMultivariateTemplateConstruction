Implement timeseries averaging, squash off-by-one bug in the initialization of empty arrays

modelbuild_averager.py
@@ -17,13 +17,7 @@ if __name__ == "__main__":
                         help="""
                         Specify a list of input files, space-separated (i.e. file1 file2 ...).
                         """)
-    parser.add_argument("--image_type", default='image', type=str,
-                        choices=['image', 'warp'],
-                        help="""
-                        Specify whether the type of image is a nifti structural image,
-                        or a set of non-linear (warp) transforms.
-                        """)
-    parser.add_argument("--method", default='efficient_trimean', type=str,
+    parser.add_argument("--method", default='mean', type=str,
                         choices=['mean', 'median', 'trimmed_mean', 'efficient_trimean', 'huber', 'sum', 'std', 'var', 'mad'],
                         help="""
                         Specify the type of average to create from the image list.
@@ -38,13 +32,22 @@ if __name__ == "__main__":
                         """)
     opts = parser.parse_args()
 
-    if len(opts.file_list)==1:
+    inputRefImage = sitk.ReadImage(opts.file_list[0])
+
+    if inputRefImage.GetDimension() == 4:
+        image_type = 'timeseries'
+    elif inputRefImage.GetNumberOfComponentsPerPixel() == 3:
+        image_type = 'warp'
+    else:
+        image_type = 'image'
+
+    if (image_type == 'image' or image_type == 'warp') and len(opts.file_list) == 1:
         print("ONLY ONE INPUT PROVIDED TO --file_list. THE OUTPUT IS THE INPUT.")
         sitk.WriteImage(sitk.ReadImage(opts.file_list[0]), opts.output)
         import sys
         sys.exit()
 
-    if opts.image_type == 'image':
+    if image_type == 'image':
         # Here we cheat to avoid loading all the images for metadata
         # make an tiny empty image, and fill in the metadata from the reader class
         img = sitk.Image([1,1,1], sitk.sitkUInt8)
@@ -105,9 +108,8 @@ if __name__ == "__main__":
 
         # Create empty array to stick data in
         # Need to reverse the dimension order b/c numpy and ITK are backwards
-        concat_array = np.empty(newdims[::-1])
-        shape = concat_array.shape
-        concat_array = concat_array.flatten()
+        concat_array = np.empty(shape=[0, np.prod(newdims[::-1])])
+        shape = newdims[::-1]
 
         for file in opts.file_list:
             if not os.path.isfile(file):
@@ -125,11 +127,24 @@ if __name__ == "__main__":
             else:
                 concat_array = np.vstack((concat_array, array.flatten()))
 
-    else:
+    elif image_type == 'timeseries':
+        # Assume all timeseries inputs are in the same space
+        concat_array = np.empty(shape=[0, np.prod(sitk.GetArrayViewFromImage(inputRefImage).shape[1:])])
+        shape = sitk.GetArrayViewFromImage(inputRefImage).shape[1:]
+        for file in opts.file_list:
+            if not os.path.isfile(file):
+                raise ValueError("The provided file {file} does not exist.".format(file=file))
+            img = sitk.ReadImage(file)
+            array = sitk.GetArrayViewFromImage(img)
+            if opts.normalize: # divide the image values by its mean
+                concat_array = np.vstack((concat_array, array.reshape(array.shape[0], -1) / array.reshape(array.shape[0], -1).mean(axis = 1, keepdims=True)))
+            else:
+                concat_array = np.vstack((concat_array, array.reshape(array.shape[0], -1)))
+
+    elif image_type == 'warp':
         # Assume all warp fields are in the same space
-        concat_array = np.empty(sitk.GetArrayViewFromImage(sitk.ReadImage(opts.file_list[0])).shape)
-        shape = concat_array.shape
-        concat_array = concat_array.flatten()
+        concat_array = np.empty(shape=[0, np.prod(inputRefImage.GetSize())*3])
+        shape = sitk.GetArrayViewFromImage(inputRefImage).shape
         for file in opts.file_list:
             if not os.path.isfile(file):
                 raise ValueError("The provided file {file} does not exist.".format(file=file))
@@ -143,7 +158,7 @@ if __name__ == "__main__":
     if opts.method == 'mean':
         average = np.mean(concat_array, axis=0)
     elif opts.method == 'median':
-        average = np.median(concat_array,axis=0)
+        average = np.median(concat_array, axis=0)
     elif opts.method == 'trimmed_mean':
         from scipy import stats
         average = stats.trim_mean(concat_array, opts.trim_percent, axis=0)
@@ -165,11 +180,16 @@ if __name__ == "__main__":
 
     average = average.reshape(shape)
 
-    if opts.image_type=='image':
+    if image_type=='image':
         average_img = sitk.GetImageFromArray(average, isVector=False)
         average_img.CopyInformation(averageRef)
         sitk.WriteImage(average_img, opts.output)
-    elif opts.image_type=='warp':
+    elif image_type=='warp':
         average_img = sitk.GetImageFromArray(average, isVector=True)
-        average_img.CopyInformation(sitk.ReadImage(opts.file_list[0]))
+        average_img.CopyInformation(inputRefImage)
+        sitk.WriteImage(average_img, opts.output)
+    elif image_type=='timeseries':
+        average_img = sitk.GetImageFromArray(average, isVector=False)
+        # Copy the image metadata from an the first extracted slice of the first image
+        average_img.CopyInformation(sitk.Extract(inputRefImage, inputRefImage.GetSize()[0:3] + tuple([0]), directionCollapseToStrategy=sitk.ExtractImageFilter.DIRECTIONCOLLAPSETOSUBMATRIX))
         sitk.WriteImage(average_img, opts.output)