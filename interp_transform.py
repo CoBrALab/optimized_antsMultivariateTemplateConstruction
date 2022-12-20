9fb51b0c3d3ae65cd10e9745f74ab9b5a52559ed
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-12-20 00:07:58 -0500

    Rewrite matrix handling using intermediate homogenous matrix, remember to copy center of rotation into output

--- interp_transform.py
+++ interp_transform.py
@@ -11,6 +11,32 @@
 import vtk
 import SimpleITK as sitk
 import argparse
+import numpy as np
+
+def itk_to_homogeneous_matrix(itk_transform):
+    # Dump the matrix 4x4 matrix
+    input_itk_transform_parameters = itk_transform.GetParameters()
+    M = np.zeros((4, 4))
+    # Extract the 3x3 matrix and place into the larger 4x4
+    M[0:3, 0:3] = np.asarray(input_itk_transform_parameters[0:9]).reshape((3, 3))
+    # Append the translation
+    M[0:3, 3] = input_itk_transform_parameters[9:]
+    M[3, 3] = 1
+
+    return M
+
+def homogenous_matrix_to_itk(M):
+    return sitk.AffineTransform(
+            M[0:3, 0:3].flatten(),
+            M[0:3, 3].flatten(),
+            (0, 0, 0),
+        )
+
+def vtk_to_homogenous_matrix(vtk_transform):
+    M = np.eye(4)
+    vtk_matrix = vtk_transform.GetMatrix()
+    vtk_matrix.DeepCopy(M.ravel(), vtk_matrix)
+    return M
 
 if __name__ == "__main__":
 
@@ -45,8 +71,7 @@ if __name__ == "__main__":
     input_itk_transform = sitk.ReadTransform(opts.input_transform)
     output_itk_transform = sitk.AffineTransform(3)
 
-    # Grab the 4x3 matrix
-    input_itk_transform_parameters = input_itk_transform.GetParameters()
+    input_itk_transform_matrix = itk_to_homogeneous_matrix(input_itk_transform)
 
     # Create VTK input/output transform, and an identity for the interpolation
     input_vtk_transform = vtk.vtkTransform()
@@ -54,15 +79,7 @@ if __name__ == "__main__":
     output_vtk_transform = vtk.vtkTransform()
 
     # Setup the VTK transform by reconstructing from the ITK matrix parameters
-    input_vtk_transform.SetMatrix(
-        input_itk_transform_parameters[0:3]
-        + input_itk_transform_parameters[9:10]
-        + input_itk_transform_parameters[3:6]
-        + input_itk_transform_parameters[10:11]
-        + input_itk_transform_parameters[6:9]
-        + input_itk_transform_parameters[11:12]
-        + (0, 0, 0, 1)
-    )
+    input_vtk_transform.SetMatrix(input_itk_transform_matrix.ravel())
 
     # Create an interpolator
     vtk_transform_interpolator = vtk.vtkTransformInterpolator()
@@ -76,22 +93,8 @@ if __name__ == "__main__":
         opts.scale_factor, output_vtk_transform
     )
 
-    # Remap VTK parameters to ITK
-    output_itk_transform.SetParameters(
-        (
-            output_vtk_transform.GetMatrix().GetElement(0, 0),
-            output_vtk_transform.GetMatrix().GetElement(0, 1),
-            output_vtk_transform.GetMatrix().GetElement(0, 2),
-            output_vtk_transform.GetMatrix().GetElement(1, 0),
-            output_vtk_transform.GetMatrix().GetElement(1, 1),
-            output_vtk_transform.GetMatrix().GetElement(1, 2),
-            output_vtk_transform.GetMatrix().GetElement(2, 0),
-            output_vtk_transform.GetMatrix().GetElement(2, 1),
-            output_vtk_transform.GetMatrix().GetElement(2, 2),
-            output_vtk_transform.GetMatrix().GetElement(0, 3),
-            output_vtk_transform.GetMatrix().GetElement(1, 3),
-            output_vtk_transform.GetMatrix().GetElement(2, 3),
-        )
-    )
+    output_itk_transform = homogenous_matrix_to_itk(vtk_to_homogenous_matrix(output_vtk_transform))
+    output_itk_transform.SetFixedParameters(input_itk_transform.GetFixedParameters())
+    print(output_itk_transform)
 
     sitk.WriteTransform(output_itk_transform, opts.output_transform)
